38a48ceb840524ec435af1b4043dbdb65d8e8382
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2023-10-24 17:48:16 +0530

    Initial commit

---
+++ src/test/java/com/boot/hotel/gateway/HotelApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.hotel.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
